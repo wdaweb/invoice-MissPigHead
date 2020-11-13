dfca14c97053de88c75322c9c3d05d133a18fdfe
MissPigHead <misspighead@gmail.com>
2020-11-13 09:18:39 +0800

    change layout, let the index as a frame of every single fuction

--- index.php
+++ index.php
@@ -8,6 +8,34 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
+        <div class="col-8 d-flex justify-content-between p-3 mx-auto border">
+            <?php
+                $month=[
+                    1=>"1,2月",
+                    2=>"3,4月",
+                    3=>"5,6月",
+                    4=>"7,8月",
+                    5=>"9,10月",
+                    6=>"11,12月",
+                ];
+                $m=ceil(date("m")/2);
+            ?>
+            <div class="text-center"><?=$month[$m];?></div>
+            <div class="text-center">
+                <a href="?do=invoice_list">當期發票</a>
+            </div>
+            <div class="text-center">
+                <a href="">對獎</a>
+            </div>
+            <div class="text-center">
+                <a href="">輸入獎號</a>
+            </div>
+        </div>
+        <div class="col-8 d-flex justify-content-between p-3 mx-auto border">
+
+        </div>
+    </div>
     <?php
         if(isset($_GET['do'])){
             $file=$_GET['do'].".php";
