a9e3b452295a538d2ab138b75a46598a1b0f2a79
MissPigHead <misspighead@gmail.com>
2020-11-13 11:57:17 +0800

    use if() & exec() to check if this one data is inserted successfully

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -3,4 +3,15 @@
 //將發票的號碼及相關資訊寫入資料庫
 
 
+include_once "../base.php";
+
+$sql="insert into invoices (`".implode("`,`",array_keys($_POST))."`) values('".implode("','",$_POST)."')";
+echo $sql;
+
+$chk = $pdo->exec($sql);
+
+if($chk){
+    header ("location:../index.php?do=invoice_list");
+}
+
 ?>
