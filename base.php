bf1d5577e081f49cc61591e5a9207f1dbeb586c9
MissPigHead <misspighead@gmail.com>
2020-11-13 08:59:35 +0800

    use base.php to avoid to type PDO info repeatedly

--- base.php
+++ base.php
@@ -2,7 +2,5 @@
         $dsn = "mysql:host=localhost;dbname=invoicesys;charset=utf8"; // DBname!!
         $pdo = new PDO($dsn,'root','');
         date_default_timezone_set("Asia/Taipei");
-        123
-        123
         session_start();
         ?>
