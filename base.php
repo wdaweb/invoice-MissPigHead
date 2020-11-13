3467e093f87d2cc82de19eac82933369ae046b05
MissPigHead <misspighead@gmail.com>
2020-11-13 08:58:46 +0800

    123

--- base.php
+++ base.php
@@ -1,8 +1,8 @@
 <?php
-        $dsn = "mysql:host=localhost;dbname=invoicesys;charset=utf8"; 
+        $dsn = "mysql:host=localhost;dbname=invoicesys;charset=utf8"; // DBname!!
         $pdo = new PDO($dsn,'root','');
-
         date_default_timezone_set("Asia/Taipei");
+        123
+        123
         session_start();
-
         ?>
