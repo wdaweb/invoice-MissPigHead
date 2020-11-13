87d7219f3649108815a0ee29fc12b678bc57b57d
MissPigHead <misspighead@gmail.com>
2020-11-13 09:19:24 +0800

    change layout, let the index as a frame of every single fuction, only keep the form in this funcion page

--- main.php
+++ main.php
@@ -1,49 +1,64 @@
 <h3 class="text-center">統一發票紀錄與對獎</h3>
-<div class="container">
-    <div class="col-8 d-flex justify-content-between p-3 mx-auto border">
-    <?php
-        $month=[
-            1=>"1,2月",
-            2=>"3,4月",
-            3=>"5,6月",
-            4=>"7,8月",
-            5=>"9,10月",
-            6=>"11,12月",
-        ];
-        $m=ceil(date("m")/2);
-    ?>
-        <div class="text-center"><?=$month[$m];?></div>
-        <div class="text-center">
-            <a href="">當期發票</a>
-        </div>
-        <div class="text-center">
-            <a href="">對獎</a>
-        </div>
-        <div class="text-center">
-            <a href="">輸入獎號</a>
-        </div>
+
+<!-- 驗證數字 -->
+<script>
+    function validateForm(form){
+        if (checkCreditCardNumber(form.cardNumber)){
+            alert("資料正確無誤，立刻送出表單！");
+            form.submit();
+            return(true);
+        }
+        alert("資料有誤，表單將不送出！");
+        form.cardNumber.focus();
+        return(false);
+    }
+
+    function checkCreditCardNumber(control){
+        var number=control.value;
+        var character;
+        var digit;
+        if (number.length!=19){
+            alert("<"+number+">：卡號長度有誤，請查核！");
+            return false;
+        }
+        for (i=0; i<19; i++){
+            character=number.charAt(i);
+            if ((i==4)||(i==9)||(i==14)){	// 檢查是否是 "-"
+                if (character!="-"){
+                    alert("<"+number+">：卡號輸入有誤（請以「-」分開四碼），請查核！");
+                    return false;
+                }
+            } else {	// 檢查是否是數字
+                if (isNaN(parseInt(character))){
+                    alert("<"+number+">：卡號輸入有誤，請查核！");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+</script>
+
+
+
+<form action="api/add_invoice.php" method="post">
+    <div>日期:<input type="date" name="payment_date"></div>
+    期別:<select name="payment_period">
+        <option value="1">1,2</option>
+        <option value="2">3,4</option>
+        <option value="3">5,6</option>
+        <option value="4">7,8</option>
+        <option value="5">9,10</option>
+        <option value="6">11,12</option>
+    </select>
+    <div>發票號碼:
+        <input type="text" name="inv_code" style="width:50px">
+        <input type="number" name="inv_number" minlength="2" maxlength="8" style="width:150px">
     </div>
-    <div class="col-8 d-flex justify-content-between p-3 mx-auto border">
-        <form action="api/add_invoice.php" method="post">
-            <div>年份:<input type="text" name="year"></div>
-            期別:<select name="period">
-                <option value="1">1,2</option>
-                <option value="2">3,4</option>
-                <option value="3">5,6</option>
-                <option value="4">7,8</option>
-                <option value="5">9,10</option>
-                <option value="6">11,12</option>
-            </select>
-            <div>發票號碼:
-                <input type="text" name="prepend" style="width:50px">
-                <input type="text" name="number" style="width:150px">
-            </div>
-            <div>
-                發票金額:<input type="number" name="payment">
-            </div>
-            <div class="text-center">
-                <input type="submit" value="送出">
-            </div>
-        </form>
+    <div>
+        發票金額:<input type="number" name="payment_amount">
     </div>
-</div>
+    <div class="text-center">
+        <input type="submit" value="送出">
+    </div>
+</form>
