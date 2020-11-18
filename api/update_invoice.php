db0d9be770b33044df85a6c547305e25776d0567
MissPigHead <misspighead@gmail.com>
2020-11-18 09:48:28 +0800

    利用if() 確認mysql update執行成功

--- api/update_invoice.php
+++ api/update_invoice.php
@@ -9,11 +9,11 @@ $sql="update invoices
     `payment_period`='{$_POST['payment_period']}',
     `payment_date`='{$_POST['payment_date']}',
     `payment_amount`='{$_POST['payment_amount']}'
-    where `id`='{$_POST['id']}'
-    ";
+    where `id`='{$_POST['id']}'";
 
 $chk=$pdo->exec($sql);
 if($chk){
     header("location：../index.php?do=invoice_list");
 }else{echo "check again";}
+
 ?>
