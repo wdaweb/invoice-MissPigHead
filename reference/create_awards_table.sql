af76abd9bc1fc5a03dc9c94b17f361d2532645c8
MissPigHead <misspighead@gmail.com>
2020-11-18 14:11:08 +0800

    change payment_year length

--- reference/create_awards_table.sql
+++ reference/create_awards_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `invoicesys`.`awards` ( 
 `id` INT(4) NOT NULL AUTO_INCREMENT , 
-`payment_year` INT(2) NOT NULL , 
+`payment_year` INT(4) NOT NULL , 
 `payment_period` INT(1) NOT NULL , 
 `inv_number` VARCHAR(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL , 
 `type` INT(1) NOT NULL , 
