e700c3bbca1371ba7fee102ffb51ad82845530d6
MissPigHead <misspighead@gmail.com>
2020-11-18 14:12:42 +0800

    將通用的獎項陣列寫入，方便引用

--- base.php
+++ base.php
@@ -1,6 +1,8 @@
 <?php
-        $dsn = "mysql:host=localhost;dbname=invoicesys;charset=utf8"; // DBname!!
-        $pdo = new PDO($dsn,'root','');
-        date_default_timezone_set("Asia/Taipei");
-        session_start();
-        ?>
+$dsn = "mysql:host=localhost;dbname=invoicesys;charset=utf8"; // DBname!!
+$pdo = new PDO($dsn,'root','');
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+$AwardStr=['頭','二','三','四','五','六'];
+?>
