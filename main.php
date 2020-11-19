320e278cca95cd0f2d420dad3f3c7526e96adc8d
MissPigHead <misspighead@gmail.com>
2020-11-19 16:36:39 +0800

    加入顯示 error message 的部分

--- main.php
+++ main.php
@@ -1,6 +1,9 @@
+<?php include_once "base.php";?>
 <form action="api/add_invoice.php" method="post">
-    <div>日期:<input type="date" name="payment_date"></div>
-    期別:<select name="payment_period">
+    <li class="list-group-item">日期:<input type="date" name="payment_date">
+    <?php errMsg('payment_date');?>
+    </li>
+    <li class="list-group-item">期別:<select name="payment_period">
         <option value="1">1,2</option>
         <option value="2">3,4</option>
         <option value="3">5,6</option>
@@ -8,14 +11,18 @@
         <option value="5">9,10</option>
         <option value="6">11,12</option>
     </select>
-    <div>發票號碼:
-        <input type="text" name="inv_code" style="width:50px" class="uppercase">
+    </li>
+    <li class="list-group-item">發票號碼:
+        <input type="text" name="inv_code" style="width:50px" class="uppercase"> - 
+        <?php errMsg('inv_code');?>
         <input type="number" name="inv_number" minlength="2" maxlength="8" style="width:150px">
-    </div>
-    <div>
+        <?php errMsg('inv_number');?>
+    </li>
+
+    <li class="list-group-item">
         發票金額:<input type="number" name="payment_amount">
-    </div>
-    <div class="text-center">
+    </li>
+    <div class="mt-2 text-center">
         <input type="submit" value="送出">
     </div>
 </form>
