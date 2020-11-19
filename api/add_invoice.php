59f7757168c13032d0a21fc51b001ddd42be37e9
MissPigHead <misspighead@gmail.com>
2020-11-19 16:37:29 +0800

    修改確認後的跳轉頁面

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -1,16 +1,16 @@
 <?php
 //撰寫新增消費發票的程式碼
 //將發票的號碼及相關資訊寫入資料庫
-
+session_start();
 
 include_once "../base.php";
 
 $sql="insert into invoices (`".implode("`,`",array_keys($_POST))."`) values('".implode("','",$_POST)."')";
 
-$chk = $pdo->exec($sql);
-
-if($chk){
+if(empty($_SESSION['err'])){
     header ("location:../index.php?do=invoice_list");
+}else{
+    header ("location:../index.php");
 }
 
 ?>
