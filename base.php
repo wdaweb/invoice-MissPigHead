9cadafaa0110f23db592f765cef11815ad79c0d4
MissPigHead <misspighead@gmail.com>
2020-11-20 16:37:44 +0800

    insert into/ update 函數未完成

--- base.php
+++ base.php
@@ -27,26 +27,8 @@ function errMsg($target){
         print_r($_SESSION['err'][$target]);}
     }
 
-// 自己練習寫，爛，重寫
-// function FindInv($table,$id,$inv_code,$inv_number,$payment_date,$payment_period,$payment_amount){
-//     global $pdo;
-//     if(!empty($id)){$where[]="`id`='$id'";}
-//     if(!empty($inv_code)){$where[]="`inv_code`='$inv_code'";}
-//     if(!empty($inv_number)){$where[]="`inv_number`='$inv_number'";}
-//     if(!empty($payment_date)){$where[]="`payment_date`='$payment_date'";}
-//     if(!empty($payment_period)){$where[]="`payment_period`='$payment_period'";}
-//     if(!empty($payment_amount)){$where[]="`payment_amount`='$payment_amount'";}
-//     $wheres=implode(" and ",$where);
-//     $sql="select * from $table where $wheres";
-//     $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-//     return $rows;
-// }
-
-
-
 
 function sqlWhere($id,$inv_code,$inv_number,$payment_date,$payment_period,$payment_amount){
-    global $pdo;
     $where=[];
     if(!empty($id)){$where[]="`id`='$id'";}
     if(!empty($inv_code)){$where[]="`inv_code`='$inv_code'";}
@@ -59,7 +41,6 @@ function sqlWhere($id,$inv_code,$inv_number,$payment_date,$payment_period,$payme
 } // like, not like... 待增加
 
 function sqlOrderBy($columnName1,$ascOrDesc1,$columnName2,$ascOrDesc2,$columnName3,$ascOrDesc3){
-    global $pdo;
     $OrderByDes="order by ".$columnName1." ".$ascOrDesc1;
     if(!empty($columnName2) && empty($columnName3)){
         $OrderByDes="order by `".$columnName1."` ".$ascOrDesc1.", `".$columnName2."` ".$ascOrDesc2;
@@ -70,23 +51,68 @@ function sqlOrderBy($columnName1,$ascOrDesc1,$columnName2,$ascOrDesc2,$columnNam
     return $OrderByDes;
 } 
 
+
+// delete or select invoice data
 function DelSelInv($delOrSel,$tableName,$whereDes,$orderByDes){
     global $pdo;
-    if($delOrSel='delete'){
-    $sqlDes="delete from `".$tableName."` ".$whereDes;
-    }elseif($delOrSel='select'){
-    $sqlDes="select * from `".$tableName."` ".$whereDes." ".$orderByDes;
+    if($delOrSel=='delete'){
+        $sqlDes="delete from `".$tableName."` ".$whereDes;
+        $pdo->query($sqlDes);
+    }elseif($delOrSel=='select'){
+        $sqlDes="select * from `".$tableName."` ".$whereDes." ".$orderByDes;
+        $invs=$pdo->query($sqlDes)->fetchAll();
+        return $invs;
     }
-    $pdo->query($sqlDes);
+}
+
+function InsUpdCoVa($inv_code,$inv_number,$payment_date,$payment_period,$payment_amount){
+    $arg[]=["`inv_code`" => "'$inv_code'"];
+    $arg[]=["`inv_number`" => "'$inv_number'"];
+    $arg[]=["`payment_date`" => "'$payment_date'"];
+    $arg[]=["`payment_period`" => "'$payment_period'"];
+    $arg[]=["`payment_amount`" => "'$payment_amount'"];
+    return $arg;
+}
+
+function InsUpdInv($InsOrUpd,$tableName,$whereDes,...$arg){
+    global $pdo;
+    foreach($arg as $columnName => $val){
+        $column[]=$columnName;
+        $value[]=$val;
+    }
+    // $t=count($value);
+    if($InsOrUpd=='insert'){
+        $columns=implode(",",$column);
+        $values=implode(",",$value);
+        $sqlDes="insert into `".$tableName."`(".$columns.") values(".$values.")";
+        print_r($sqlDes);
+    //     $pdo->query($sqlDes);
+    // }elseif($delOrSel=='select'){
+    //     $sqlDes="select * from `".$tableName."` ".$whereDes." ".$orderByDes;
+    //     $invs=$pdo->query($sqlDes)->fetchAll();
+    //     return $invs;
+    }
 }
 
 
+// function all($table,...$arg){
+//     global $pdo;
+//     $sql="select * from $table ";
 
+//     if(isset($arg[0])){
+    
+//         if(is_array($arg[0])){
+//             //製作會在 where 後面的句子字串(陣列格式)
+//                 foreach($arg[0] as $key => $value){
+//                     $tmp[]=sprintf("`%s`='%s'",$key,$value);
+//                 }
 
-
-
-
-
-
+//                 $sql=$sql." where ".implode(' && ',$tmp);
+            
+//         }else{
+//             //製作非陣列的語句接在$sql後面
+//                 $sql=$sql.$arg[0];       
+//         }
+//     }
 
 ?>
