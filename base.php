ba69b603877f2dea9077f6624d3bb8c9977b5bbe
MissPigHead <misspighead@gmail.com>
2020-11-20 15:00:59 +0800

    增加3個自訂函數，刪除/搜尋發票，含where條件 及 order by 排序

--- base.php
+++ base.php
@@ -27,29 +27,66 @@ function errMsg($target){
         print_r($_SESSION['err'][$target]);}
     }
 
-// 自己練習寫
-function FindInv($table,$id,$inv_code,$inv_number,$payment_date,$payment_period,$payment_amount){
+// 自己練習寫，爛，重寫
+// function FindInv($table,$id,$inv_code,$inv_number,$payment_date,$payment_period,$payment_amount){
+//     global $pdo;
+//     if(!empty($id)){$where[]="`id`='$id'";}
+//     if(!empty($inv_code)){$where[]="`inv_code`='$inv_code'";}
+//     if(!empty($inv_number)){$where[]="`inv_number`='$inv_number'";}
+//     if(!empty($payment_date)){$where[]="`payment_date`='$payment_date'";}
+//     if(!empty($payment_period)){$where[]="`payment_period`='$payment_period'";}
+//     if(!empty($payment_amount)){$where[]="`payment_amount`='$payment_amount'";}
+//     $wheres=implode(" and ",$where);
+//     $sql="select * from $table where $wheres";
+//     $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+//     return $rows;
+// }
+
+
+
+
+function sqlWhere($id,$inv_code,$inv_number,$payment_date,$payment_period,$payment_amount){
     global $pdo;
+    $where=[];
     if(!empty($id)){$where[]="`id`='$id'";}
     if(!empty($inv_code)){$where[]="`inv_code`='$inv_code'";}
     if(!empty($inv_number)){$where[]="`inv_number`='$inv_number'";}
     if(!empty($payment_date)){$where[]="`payment_date`='$payment_date'";}
     if(!empty($payment_period)){$where[]="`payment_period`='$payment_period'";}
     if(!empty($payment_amount)){$where[]="`payment_amount`='$payment_amount'";}
-    $wheres=implode(" and ",$where);
-    $sql="select * from $table where $wheres";
-    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    return $rows;
+    $whereDes="where ".implode(" and ",$where);
+    return $whereDes;
+} // like, not like... 待增加
+
+function sqlOrderBy($columnName1,$ascOrDesc1,$columnName2,$ascOrDesc2,$columnName3,$ascOrDesc3){
+    global $pdo;
+    $OrderByDes="order by ".$columnName1." ".$ascOrDesc1;
+    if(!empty($columnName2) && empty($columnName3)){
+        $OrderByDes="order by `".$columnName1."` ".$ascOrDesc1.", `".$columnName2."` ".$ascOrDesc2;
+    }
+    if(!empty($columnName3)){
+        $OrderByDes="order by `".$columnName1."` ".$ascOrDesc1.", `".$columnName2."` ".$ascOrDesc2.", `".$columnName3."` ".$ascOrDesc3;
+    }
+    return $OrderByDes;
+} 
+
+function DelSelInv($delOrSel,$tableName,$whereDes,$orderByDes){
+    global $pdo;
+    if($delOrSel='delete'){
+    $sqlDes="delete from `".$tableName."` ".$whereDes;
+    }elseif($delOrSel='select'){
+    $sqlDes="select * from `".$tableName."` ".$whereDes." ".$orderByDes;
+    }
+    $pdo->query($sqlDes);
 }
 
 
-// 參考老師
 
-function find($table,$def){
-    global $pdo;
-    $sql="select * from $table where $def";
-    $row=$pdo -> query($sql) -> fetch(PDO::FETCH_ASSOC); 
-    return $row;}
+
+
+
+
+
 
 
 ?>
