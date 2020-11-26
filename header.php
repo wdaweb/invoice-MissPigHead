4e4878fad50329db3a66d412a21b8678ad1ea242
MissPigHead <47239947+MissPigHead@users.noreply.github.com>
2020-11-26 17:24:49 +0800

    use haeder.php & footer.php to bulid the page

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
+      <?php
+        if(empty($title)){
+          $title="統一發票紀錄及對獎";
+        }
+        echo $title;
+      ?>
+    </title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/custom.css">
+</head>
