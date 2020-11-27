e74126b91d6868a89c767e853811428a284f230c
MissPigHead <47239947+MissPigHead@users.noreply.github.com>
2020-11-27 08:22:16 +0800

    加自定函數

--- base.php
+++ base.php
@@ -3,21 +3,21 @@ $pdo=new PDO("mysql:host=localhost;dbname=invoicesys;charset=utf8",'root','');
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
-function actSQL($action,$tableName,$updateData,$whereDes){
+function actSQL($action,$tableName,$columnValue,$whereDes){
   global $pdo;
   if(!empty($whereDes)){
     foreach($whereDes as $c => $v){
       $where[]="`$c`='$v'";
-      $columns[]=$c;
-      $values[]=$v;
     }
     $whereDes="where ".implode("&&", $where);
   }else{
     $whereDes='';
   }
-  if(!empty($updateData)){
-    foreach($updateData as $c => $v){
+  if(!empty($columnValue)){
+    foreach($columnValue as $c => $v){
       $set[]="`$c`='$v'";
+      $columns[]=$c;
+      $values[]=$v;
     }
     $setDes="set ".implode(",", $set);
   }else{
@@ -26,22 +26,30 @@ function actSQL($action,$tableName,$updateData,$whereDes){
   switch ($action){
     case 'select':
       $sql="select * from `".$tableName."` ".$whereDes;
+      print_r($sql);
       return $pdo->query($sql)->fetchAll();
     break;
-    case 'add':
-      $sql="insert into `".$tableName."`(`".implode("`,`",$columns)."`) values('".implode("','",$values);  
-      return $pdo->exec($sql);
+    case 'insert':
+      $sql="insert into `".$tableName."`(`".implode("`,`",$columns)."`) values('".implode("','",$values)."')";  
+      print_r($sql);
+      // return 
+      $pdo->exec($sql);
     break;
     case 'delete':
       $sql="delete from `".$tableName."` ".$whereDes;
-      return $pdo->exec($sql);
+      print_r($sql);
+      // return 
+      $pdo->exec($sql);
     break;
     case 'update':
       $sql="update `".$tableName." ".$setDes." ".$whereDes;
+      print_r($sql);
       return $pdo->exec($sql);
     break;
   }
 }
 
-
+function go($url){
+  header("location:".$url);
+}
 ?>
