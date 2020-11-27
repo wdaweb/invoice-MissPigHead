d2bed033510ea19a42416cfd1bdd79f6d17107d3
MissPigHead <47239947+MissPigHead@users.noreply.github.com>
2020-11-27 08:18:17 +0800

    整理input 欄位

--- signup.php
+++ signup.php
@@ -1,28 +1,35 @@
-<form id="register" class="container" action="register.php" method="post">
+<!-- 資料格式驗證：待老師教JavaScipt後補充 -->
+<form class="container" action="./api/add_user.php" method="post">
   <div class="row justify-content-center">
     <div class="input-group col-sm-10 col-md-8 col-lg-6 mx-md-5 my-2">
       <div class="input-group-prepend">
         <span class="input-group-text">帳號</span>
       </div>
-      <input type="text" class="form-control" name="account" placeholder="請輸入4~10字元英文或數字">
+      <input type="text" class="form-control" name="acc" placeholder="請輸入6~10字元英文或數字" required>
     </div>
     <div class="input-group col-sm-10 col-md-8 col-lg-6 mx-md-5 my-2">
       <div class="input-group-prepend">
         <span class="input-group-text">密碼</span>
       </div>
-      <input type="password" class="form-control" name="password" placeholder="請輸入8~16字元英文或數字">
+      <input type="password" class="form-control" name="pw" placeholder="請輸入8~16字元英文或數字" required>
+    </div>
+    <div class="input-group col-sm-10 col-md-8 col-lg-6 mx-md-5 my-2">
+      <div class="input-group-prepend">
+        <span class="input-group-text">確認密碼</span>
+      </div>
+      <input type="password" class="form-control" name="pw2" placeholder="請重新輸入密碼" required>
     </div>
     <div class="input-group col-sm-10 col-md-8 col-lg-6 mx-md-5 my-2">
       <div class="input-group-prepend">
         <span class="input-group-text">生日</span>
       </div>
-      <input type="date" class="form-control" name="birthday">
+      <input type="date" class="form-control" name="birth">
     </div>
     <div class="input-group col-sm-10 col-md-8 col-lg-6 mx-md-5 my-2">
       <div class="input-group-prepend">
         <span class="input-group-text">手機</span>
       </div>
-      <input type="number" class="form-control" name="tel">
+      <input type="number" class="form-control" name="tel" pattern="[0-9]{10}">
     </div>
     <div class="input-group col-sm-10 col-md-8 col-lg-6 mx-md-5 my-2">
       <div class="input-group-prepend">
