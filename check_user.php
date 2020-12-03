cd91fe77b3c7106199315f5bfe6c8ef7ddfdd4b8
MissPigHead <47239947+MissPigHead@users.noreply.github.com>
2020-12-04 02:47:26 +0800

    test session[] & terrible user-defined function.. need to check again

--- check_user.php
+++ check_user.php
@@ -1,6 +1,7 @@
 <?php
 include_once("base.php");
-$_SESSION['err'][]='';
+
+$_SESSION['err']=[];
 
 $whereDes=whereDes(whereD1('','acc',$_POST['acc'],'','',''),'','','');
 print_r($whereDes);
@@ -8,16 +9,13 @@ print_r($whereDes);
 $rows=actSQL('select','user','',$whereDes); // 資料庫叫出X筆資料
 foreach($rows as $row){} // 將該筆資料，轉成陣列
 
-if(empty($row['acc'])){
-  $_SESSION['err']['acc']['notExist']="此帳號不存在";
-  go("./index.php?do=login");
-}elseif(!empty($row['acc']) && $row['acc']!==$_POST['acc']){
-  $_SESSION['err']['acc']['notExist']="此帳號不存在";
+if(empty($row['acc']) || (!empty($row['acc']) && $row['acc']!==$_POST['acc'])){
+  $_SESSION['err']['acc']="此帳號不存在";
   go("./index.php?do=login");
 }elseif($_POST['pw']!==$row['pw']){
-  $_SESSION['err']['pw']['wrong']="密碼錯誤";
+  $_SESSION['err']['pw']="密碼錯誤";
   go("./index.php?do=login");
-}elseif($_POST['pw']===$row['pw']){
+}else{
   $_SESSION['id']=$row['id'];
   $_SESSION['acc']=$row['acc'];
   go("./index.php?do=welcome");
