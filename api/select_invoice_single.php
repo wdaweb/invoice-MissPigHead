dbdf7e0ae6064261c5b2c0e951ab0d202104d30f
MissPigHead <47239947+MissPigHead@users.noreply.github.com>
2020-12-05 01:42:39 +0800

    改用GET 傳inv id

--- api/select_invoice_single.php
+++ api/select_invoice_single.php
@@ -1,6 +1,6 @@
 <?php
 include_once "../base.php";
-$sql="SELECT * FROM `invoice` WHERE `id`='{$_POST['id']}' && `user_id`='{$_SESSION['id']}'";
+$sql="SELECT * FROM `invoice` WHERE `id`='{$_GET['id']}' && `user_id`='{$_SESSION['id']}'";
 $rows=selectSQL($sql);
 $_SESSION['selectedInv']=$rows[0];
 // print_r($_POST);
