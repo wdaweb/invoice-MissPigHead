e956658f0fce411186966061d00b9703070a8032
MissPigHead <misspighead@gmail.com>
2020-12-04 16:38:56 +0800

    edit single invoice

--- edit_invoice.php
+++ edit_invoice.php
@@ -9,33 +9,33 @@
         <div class="input-group-prepend">
           <span class="input-group-text">發票字軌</span>
         </div>
-        <input type="text" class="form-control" name="code" placeholder="<?=$inv['code'];?>">
+        <input type="text" class="form-control" name="code" value="<?=$inv['code'];?>">
       </div>
       <div class="input-group col-md-7 mx-0 px-0">
         <div class="input-group-prepend">
           <span class="input-group-text">號碼</span>
         </div>
-        <input type="text" class="form-control" name="num" placeholder="<?=$inv['num'];?>">
+        <input type="text" class="form-control" name="num" value="<?=$inv['num'];?>">
       </div>
     </div>
     <div class="input-group col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-md-5 px-0 my-2">
       <div class="input-group-prepend">
         <span class="input-group-text">發票日期</span>
       </div>
-      <input type="date" class="form-control" name="date" placeholder="<?=$inv['date'];?>">
+      <input type="date" class="form-control" name="date" value="<?=$inv['date'];?>">
     </div>
     <div class="input-group col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-md-5 px-0 my-2">
       <div class="input-group-prepend">
         <span class="input-group-text">發票年度</span>
       </div>
-      <input type="number" class="form-control" name="year" placeholder="<?=$inv['year'];?>">
+      <input type="number" class="form-control" name="year" value="<?=$inv['year'];?>">
     </div>
     <div class="input-group col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-md-5 px-0 my-2">
       <div class="input-group-prepend">
         <span class="input-group-text">對獎月份</span>
       </div>
       <select type="number" class="form-control" name="period">
-      <option value=""><?=$inv['period'];?></option>
+      <option value="<?=$inv['period'];?>"></option>
         <option value="1">01 ~ 02 月</option>
         <option value="2">03 ~ 04 月</option>
         <option value="3">05 ~ 06 月</option>
@@ -48,13 +48,13 @@
       <div class="input-group-prepend">
         <span class="input-group-text">發票金額</span>
       </div>
-      <input type="text" class="form-control" name="amount" placeholder="<?=$inv['amount'];?>">
+      <input type="text" class="form-control" name="amount" value="<?=$inv['amount'];?>">
     </div>
     <div class="input-group col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-md-5 px-0 my-2">
       <div class="input-group-prepend">
         <span class="input-group-text">消費類型</span>
       </div>
-      <select type="number" class="form-control" name="type" placeholder="<?=$inv['type'];?>">
+      <select type="number" class="form-control" name="type" value="<?=$inv['type'];?>">
         <option value="">- 請選擇 -</option>
         <option value="1">餐飲、食物</option>
         <option value="2">衣物、鞋、包、錶</option>
@@ -71,13 +71,13 @@
       <div class="input-group-prepend">
         <span class="input-group-text">消費商家</span>
       </div>
-      <input type="text" class="form-control" name="store" placeholder="<?=$inv['store'];?>">
+      <input type="text" class="form-control" name="store" value="<?=$inv['store'];?>">
     </div>
     <div class="input-group col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-md-5 px-0 my-2">
       <div class="input-group-prepend">
         <span class="input-group-text">其他備註</span>
       </div>
-      <input type="text" class="form-control" name="other" placeholder="<?=$inv['other'];?>">
+      <input type="text" class="form-control" name="other" value="<?=$inv['other'];?>">
     </div>
     <!-- acc 帳號 隱藏欄位 <input type="number" class="form-control" name="acc"> -->
     <div class="col-6 my-2 d-flex justify-content-center">
