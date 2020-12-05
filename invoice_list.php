bb1d10583813e28fe467c348f5893a8bb63ec437
MissPigHead <47239947+MissPigHead@users.noreply.github.com>
2020-12-05 23:21:37 +0800

    add input columns for year/period, fix period chenise desciption index issue

--- invoice_list.php
+++ invoice_list.php
@@ -2,6 +2,7 @@
   include_once "./base.php";
   $year=$_SESSION['year'];
   $period=$_SESSION['period'];
+  $periodCH=$_SESSION['periodCH'];
   $pLine=$_SESSION['pLine'];
   $page=$_SESSION['page'];
   $invs=$_SESSION['invs'];
@@ -12,11 +13,37 @@
   $pNum=$_SESSION['pNum'];
   $pNext=$_SESSION['pNext']; // 下頁頁數
   $pPre=$_SESSION['pPre']; // 上頁頁數
-
 ?>
 
+<div class="container text-center" id="invoiceList">
+<form action="./api/insert_invoice.php" method="post" >
+  <div class="row justify-content-center mx-1 mx-sm-3 my-1 my-sm-2">
+    <div class="input-group col-4 col-md-3 px-0">
+      <div class="input-group-prepend">
+        <span class="input-group-text px-1 px-sm-2 px-md-3 text-07">年度</span>
+      </div>
+      <input type="number" class="form-control px-1 px-sm-2 px-md-3 text-07" name="year" value="<?=$year;?>">
+    </div>
+    <div class="input-group col-6 col-md-4 px-0">
+      <div class="input-group-prepend">
+        <span class="input-group-text px-1 px-sm-2 px-md-3 text-07">月份</span>
+      </div>
+      <select type="number" class="form-control px-1 px-sm-2 px-md-3 text-07" name="period">
+      <option value="<?=$period;?>"><?=$periodCH[$period];?></option>
+        <option value="1">01 ~ 02 月</option>
+        <option value="2">03 ~ 04 月</option>
+        <option value="3">05 ~ 06 月</option>
+        <option value="4">07 ~ 08 月</option>
+        <option value="5">09 ~ 10 月</option>
+        <option value="6">11 ~ 12 月</option>
+      </select>
+    </div>
+    <div class="input-group col-2 px-0">
+      <button type="submit" class="btn btn-info px-1 px-sm-2 px-md-3">送出</button>
+    </div>
+  </div>
+</form>  
 
-<div class="container text-center">
   <ul class="list-group row mx-lg-3 list-group-horizontal font-weight-bolder">
   <!-- 列出表頭 -->
     <?php
