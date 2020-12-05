cb389cfda3169e6c45a11dc399f2ccc2a2f1275e
MissPigHead <47239947+MissPigHead@users.noreply.github.com>
2020-12-05 23:31:32 +0800

    change file path to ./api/ , change some function name

---
+++ api/check_user.php
@@ -0,0 +1,23 @@
+<?php
+include_once("../base.php");
+
+$_SESSION['err']=[];
+
+$whereDes=whereDes(whereD1('','acc',$_POST['acc'],'','',''),'','','');
+
+$rows=actSQL('select','user','',$whereDes); // 資料庫叫出X筆資料
+foreach($rows as $row){} // 將該筆資料，轉成陣列
+
+if(empty($row['acc']) || (!empty($row['acc']) && $row['acc']!==$_POST['acc'])){
+  $_SESSION['err']['login']['acc']="此帳號不存在";
+  go("./index.php?do=login");
+}elseif($_POST['pw']!==$row['pw']){
+  $_SESSION['err']['login']['pw']="密碼錯誤";
+  go("./index.php?do=login");
+}else{
+  $_SESSION['id']=$row['id'];
+  $_SESSION['acc']=$row['acc'];
+  go("../index.php?do=welcome");
+}
+
+?>
